b874e59461db0f4526b41f8135993f4ed8caa23a
alnahlaw <hadi.alnahlawi@mail.utoronto.ca>
2019-03-21 22:42:50 -0400

    changed some code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://abdelhadi-n@hotmail.com:ahAH1011@ds221416.mlab.com:21416/heroku_lpk74b29";
+const uri = "mongodb://hadi:ahAH1011@ds221416.mlab.com:21416/heroku_lpk74b29";
 const client = new MongoClient(encodeURI(uri), { useNewUrlParser: true });
 
 
